7d18b79ab45f41f3dbe0348745bb5b2650633ad6
Punjadharani <107171262+Punjadharani@users.noreply.github.com>
2022-06-09 12:45:54 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,8 @@
+	#To find factorial of a number using recursive function
+def factorial(a):
+        if a==0:
+                return 1
+        else:
+                return(a*factorial(a-1))
+x=int(input("Enter the number"))
+print("Factorial of {} is {}".format(x,factorial(x)))
